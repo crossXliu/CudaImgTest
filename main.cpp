94114e742b5b561fab929e592e22ec213ef61cc5
moa <liuweijian_dianqi@163.com>
2023-12-14 13:45:18 +0800

    change the cpp standard into c++14 and use cmake

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string_view>
 #include <type_traits>
 #include <vector>
 #include <tuple>
@@ -13,11 +12,19 @@ enum Fmt {
 };
 
 struct CudaCase {
-    constexpr CudaCase(Fmt iFmt, int w, int h, std::string_view inSv) : inputFmt(iFmt), w(w), h(h), inputPath(inSv) {}
-    enum Fmt inputFmt;
-    int w;
-    int h;
-    std::string_view inputPath;
+  constexpr CudaCase(Fmt inFmt, int inputWidth, int inputHeight,
+                     const char *inputPath, Fmt outFmt, int outputWidth,
+                     int outputHeight, const char *outputPath)
+      : inputFmt(inFmt), inputWidth(inputWidth), inputHeight(inputHeight), inputPath(inputPath),
+      outputFmt(outFmt), outputWidth(outputWidth), outputHeight(outputHeight), outputPath(outputPath) {}
+  enum Fmt inputFmt;
+  int inputWidth;
+  int inputHeight;
+  enum Fmt outputFmt;
+  int outputWidth;
+  int outputHeight;
+  const char *inputPath;
+  const char *outputPath;
 };
 
 template <Fmt ftype>
@@ -45,26 +52,31 @@ constexpr auto genCases(Fmt f) {
 template <typename Tuple>
 constexpr auto describeCases(Tuple t)
 {
-    return t;
+    return std::make_tuple(t);
 }
 
 template <typename TupleHead, typename... Tuple>
 constexpr auto describeCases(TupleHead h, Tuple... t)
 {
-    return std::tuple_cat(std::make_tuple(h), std::make_tuple(describeCases<Tuple...>(t...)));
+    return std::tuple_cat(std::make_tuple(h), describeCases<Tuple...>(t...));
 }
 
-template <typename TCaseContainer, size_t idx = 0, std::enable_if_t<idx == std::tuple_size_v<decltype(TCaseContainer::describeInstance())>,
+template <typename TCaseContainer, size_t idx = 0, std::enable_if_t<idx == std::tuple_size<decltype(TCaseContainer::describeInstance())>::value,
                                        void *> = nullptr>
 void runCases(std::vector<float> &res) {}
 
-template <typename TCaseContainer, size_t idx = 0, std::enable_if_t<idx < std::tuple_size_v<decltype(TCaseContainer::describeInstance())>, void*> = nullptr>
+template <typename TCaseContainer, size_t idx = 0, std::enable_if_t<idx < std::tuple_size<decltype(TCaseContainer::describeInstance())>::value, void*> = nullptr>
 void runCases(std::vector<float>& res)
 {
     auto fmtFactorIn = FmtFactor<std::get<idx>(TCaseContainer::describeInstance()).inputFmt>::val;
     auto inputPath = std::get<idx>(TCaseContainer::describeInstance()).inputPath;
-    auto w = std::get<idx>(TCaseContainer::describeInstance()).w;
-    auto h = std::get<idx>(TCaseContainer::describeInstance()).h;
+    auto inputWidth = std::get<idx>(TCaseContainer::describeInstance()).inputWidth;
+    auto inputHeight = std::get<idx>(TCaseContainer::describeInstance()).inputHeight;
+
+    auto fmtFactorOut = FmtFactor<std::get<idx>(TCaseContainer::describeInstance()).outputFmt>::val;
+    auto outputPath = std::get<idx>(TCaseContainer::describeInstance()).outputPath;
+    auto outputWidth = std::get<idx>(TCaseContainer::describeInstance()).outputWidth;
+    auto outputHeight = std::get<idx>(TCaseContainer::describeInstance()).outputHeight;
 
     // the main function is here
     res.push_back(fmtFactorIn);
@@ -76,17 +88,18 @@ void runCases(std::vector<float>& res)
 class CasesContainer {
 public:
     constexpr static auto describeInstance() {
-        return describeCases(CudaCase(YUV, 1920, 1080, "/home/moa/.cfg"),
-                CudaCase(RGB, 1920, 1080, "/home/moa/.cfg"));
+        return describeCases(CudaCase(YUV, 1920, 1080, "/home/moa/.cfg", YUV, 1920, 1080, ""));
     }
 };
 
 class CasesContainerLDC {
 public:
-    constexpr static auto describeInstance() {
-        return describeCases(CudaCase(RGB, 1920, 1080, "/home/moa/.cfg"),
-                CudaCase(RGB, 1920, 1080, "/home/moa/.cfg"));
-    }
+  constexpr static auto describeInstance() {
+    return describeCases(
+        CudaCase(YUV, 1920, 1080, "/home/moa/.cfg", YUV, 1920, 1080, ""),
+        CudaCase(RGB, 1920, 1080, "/home/moa/.cfg", YUV, 1920, 1080, ""),
+        CudaCase(YUV, 1920, 1080, "/home/moa/.cfg", RGB, 1920, 1080, ""));
+  }
 };
 
 int main(int argc, char *argv[]) {
