ba226cb0710741d4605ddfebc9e3f9a1d2dca824
moa <liuweijian_dianqi@163.com>
2023-12-14 10:37:07 +0800

    adding tuple and type

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string_view>
 #include <type_traits>
 #include <vector>
 #include <tuple>
@@ -6,18 +7,17 @@
 using std::cout;
 using std::endl;
 
-template <typename T> class CBase {
-public:
-  explicit CBase() {}
-  virtual ~CBase() {}
-  void print() { std::cout << "hello world" << std::endl; }
-
-  template <T N> void print() { std::cout << "print val " << N << std::endl; }
-};
-
 enum Fmt {
     YUV = 0,
     RGB = 1,
+};
+
+struct CudaCase {
+    constexpr CudaCase(Fmt iFmt, int w, int h, std::string_view inSv) : inputFmt(iFmt), w(w), h(h), inputPath(inSv) {}
+    enum Fmt inputFmt;
+    int w;
+    int h;
+    std::string_view inputPath;
 };
 
 template <Fmt ftype>
@@ -38,25 +38,61 @@ public:
     constexpr static float val = 3.0F;
 };
 
-constexpr Fmt g_ftype[]{YUV, YUV, RGB};
+constexpr auto genCases(Fmt f) {
+    return std::make_tuple(f);
+}
 
-template <size_t idx = 0, std::enable_if_t<idx == sizeof(g_ftype) / sizeof(Fmt),
+template <typename Tuple>
+constexpr auto describeCases(Tuple t)
+{
+    return t;
+}
+
+template <typename TupleHead, typename... Tuple>
+constexpr auto describeCases(TupleHead h, Tuple... t)
+{
+    return std::tuple_cat(std::make_tuple(h), std::make_tuple(describeCases<Tuple...>(t...)));
+}
+
+template <typename TCaseContainer, size_t idx = 0, std::enable_if_t<idx == std::tuple_size_v<decltype(TCaseContainer::describeInstance())>,
                                        void *> = nullptr>
 void runCases(std::vector<float> &res) {}
 
-template <size_t idx = 0, std::enable_if_t<idx < sizeof(g_ftype) / sizeof(Fmt), void*> = nullptr>
+template <typename TCaseContainer, size_t idx = 0, std::enable_if_t<idx < std::tuple_size_v<decltype(TCaseContainer::describeInstance())>, void*> = nullptr>
 void runCases(std::vector<float>& res)
 {
-    res.push_back(FmtFactor<g_ftype[idx]>::val);
-    runCases<idx + 1>(res);
+    auto fmtFactorIn = FmtFactor<std::get<idx>(TCaseContainer::describeInstance()).inputFmt>::val;
+    auto inputPath = std::get<idx>(TCaseContainer::describeInstance()).inputPath;
+    auto w = std::get<idx>(TCaseContainer::describeInstance()).w;
+    auto h = std::get<idx>(TCaseContainer::describeInstance()).h;
+
+    // the main function is here
+    res.push_back(fmtFactorIn);
+
+    // run next case recursively
+    runCases<TCaseContainer, idx + 1>(res);
 }
 
-int main(int argc, char *argv[]) {
-  CBase<int> obj;
-  obj.template print<200>();
+class CasesContainer {
+public:
+    constexpr static auto describeInstance() {
+        return describeCases(CudaCase(YUV, 1920, 1080, "/home/moa/.cfg"),
+                CudaCase(RGB, 1920, 1080, "/home/moa/.cfg"));
+    }
+};
 
+class CasesContainerLDC {
+public:
+    constexpr static auto describeInstance() {
+        return describeCases(CudaCase(RGB, 1920, 1080, "/home/moa/.cfg"),
+                CudaCase(RGB, 1920, 1080, "/home/moa/.cfg"));
+    }
+};
+
+int main(int argc, char *argv[]) {
   std::vector<float> res;
-  runCases(res);
+  runCases<CasesContainer>(res);
+  runCases<CasesContainerLDC>(res);
 
   for (auto val : res) {
     cout << val << endl;
